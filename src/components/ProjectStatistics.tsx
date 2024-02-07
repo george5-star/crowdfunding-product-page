45be37ba652cef0359a446716baeb87c66da7062
George Asiedu <georgeasiedu543@gmail.com>
2024-02-07 10:25:47 +0000

    Update ProjectStatistics component with new statistics display

--- src/components/ProjectStatistics.tsx
+++ src/components/ProjectStatistics.tsx
@@ -1,5 +1,22 @@
 const ProjectStatistics = () => {
-  return <div className="container mx-auto text-center">ProjectStatistics</div>;
+  return (
+    <div className="container mx-auto text-center">
+      <div>
+        <div>
+          <h2>$89,914</h2>
+          <p>of $100,000 backed</p>
+        </div>
+        <div>
+          <h2>5,007</h2>
+          <p>total backers</p>
+        </div>
+        <div>
+          <h2>56</h2>
+          <p>days left</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectStatistics;
