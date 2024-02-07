cd3817d1afc3e545f6cc91f3c4170cff294486de
George Asiedu <georgeasiedu543@gmail.com>
2024-02-07 12:24:03 +0000

    Add data.json, Pledge Component and ProjectDescription Component

---
+++ src/components/Pledge.tsx
@@ -0,0 +1,19 @@
+import data from "../../data.json";
+
+const Pledge = () => {
+  return (
+    <div>
+      <div>
+        <h2></h2>
+        <p>{`Pledge - or more`}</p>
+      </div>
+      <p></p>
+      <div>
+        <p></p>
+        <button>Select Reward</button>
+      </div>
+    </div>
+  );
+};
+
+export default Pledge;
